b2a4b39800cb372e041ebbf338887ac15fff0606
Mojitaba34 <mojtabadavi14@gmail.com>
2024-01-31 20:50:49 +0330

    feat (users/CU-86drd34np): separate manager from user model module

---
+++ apps/user/manager.py
@@ -0,0 +1,35 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import gettext_lazy as _
+
+from apps.core.enums import UserType
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, phone_number, password, **extra_fields):
+
+        if not phone_number:
+            raise ValueError(_("The phone number must be set"))
+        if not password:
+            raise ValueError(_("The password must be set"))
+        user = self.model(phone_number=phone_number, **extra_fields)
+        user.set_password(password)
+        # TODO set referral percent based on admin setting
+        if not user.is_superuser:
+            user.referral_percent = 10
+
+        user.save()
+        return user
+
+    def create_superuser(self, phone_number, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+        extra_fields.setdefault("name", "Superuser")
+        extra_fields.setdefault("type", UserType.ADMIN)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("Superuser must have is_staff=True."))
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("Superuser must have is_superuser=True."))
+
+        return self.create_user(phone_number, password, **extra_fields)
--- apps/user/models.py
+++ apps/user/models.py
@@ -3,7 +3,6 @@ import uuid
 from django.contrib.auth.models import (
     AbstractBaseUser,
     PermissionsMixin,
-    BaseUserManager,
 )
 from django.db import models
 from django.utils.translation import gettext_lazy as _
@@ -11,37 +10,7 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 from apps.core.enums import UserType
 from apps.core.mixins.models import CustomBaseModel
-
-
-class UserManager(BaseUserManager):
-    def create_user(self, phone_number, password, **extra_fields):
-
-        if not phone_number:
-            raise ValueError(_("The phone number must be set"))
-        if not password:
-            raise ValueError(_("The password must be set"))
-        user = self.model(phone_number=phone_number, **extra_fields)
-        user.set_password(password)
-        # TODO set referral percent based on admin setting
-        if not user.is_superuser:
-            user.referral_percent = 10
-
-        user.save()
-        return user
-
-    def create_superuser(self, phone_number, password, **extra_fields):
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-        extra_fields.setdefault("is_active", True)
-        extra_fields.setdefault("name", "Superuser")
-        extra_fields.setdefault("type", UserType.ADMIN)
-
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError(_("Superuser must have is_staff=True."))
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError(_("Superuser must have is_superuser=True."))
-
-        return self.create_user(phone_number, password, **extra_fields)
+from apps.user.manager import UserManager
 
 
 class User(AbstractBaseUser, CustomBaseModel, PermissionsMixin):
