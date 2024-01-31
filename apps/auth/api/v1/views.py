c2bee65a44f843263b4b44954e22cd6e21180af8
Mojitaba34 <mojtabadavi14@gmail.com>
2024-01-31 22:05:24 +0330

    refactor (auth): Run formatter and used type hinting

--- apps/auth/api/v1/views.py
+++ apps/auth/api/v1/views.py
@@ -1,15 +1,18 @@
+from rest_framework.permissions import AllowAny
+from rest_framework.request import Request
 from rest_framework.views import APIView
 
+from apps.auth.api.v1.serializers import UserSignUpSerializer
 from apps.core.responses import CreateResponse
-from apps.user.api.v1.serializers import UserSerializer
 
 
 class SignupView(APIView):
-    serializer_class = UserSerializer
+    serializer_class = UserSignUpSerializer
     authentication_classes = ()
+    permission_classes = (AllowAny,)
 
-    def post(self, request):
-        serializer = self.serializer_class(data=request.data)
+    def post(self, request: Request):
+        serializer: UserSignUpSerializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return CreateResponse(message="User created", data=serializer.data)
