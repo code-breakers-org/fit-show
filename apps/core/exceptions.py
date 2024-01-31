6e7a92ebdefedd1c68b1245bf42c447fb39ba28e
Mojitaba34 <mojtabadavi14@gmail.com>
2024-01-31 20:47:37 +0330

    fix (exceptions/CU-86drd34np): run formatter

--- apps/core/exceptions.py
+++ apps/core/exceptions.py
@@ -1,5 +1,4 @@
 import logging
-import traceback
 import uuid
 
 from django.conf import settings
@@ -8,7 +7,6 @@ from rest_framework.exceptions import APIException
 from rest_framework.views import exception_handler
 
 from apps.core.responses import ErrorResponse
-from django.conf import settings
 
 logger = logging.getLogger(settings.LOGGER_NAME)
 
@@ -30,7 +28,8 @@ def custom_exception_handler(exc: Exception, context: dict):
             exc_info=exc,
         )
         return ErrorResponse(
-            message=f"Unexpected Error with error id of {exc_id}", status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            message=f"Unexpected Error with error id of {exc_id}",
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
         )
     return response
 
@@ -41,7 +40,7 @@ class CustomAPIException(APIException):
         error_message="Error",
         error_data=None,
         status_code=status.HTTP_400_BAD_REQUEST,
-        **kwargs
+        **kwargs,
     ):
         self.error_message = error_message
         self.error_data = error_data
