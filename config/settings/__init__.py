6adbc9ded92ef468b4a82062b5084a9b26843e4e
M.Daneshjoo <mohammad.daneshjoo92@gmail.com>
2024-02-05 16:17:10 +0330

    fix(logger): export logger setting

--- config/settings/__init__.py
+++ config/settings/__init__.py
@@ -3,4 +3,5 @@ from config.settings.base import *
 from config.settings.db import *
 from config.settings.drf import *
 from config.settings.files import *
+from config.settings.log import *
 from config.settings.swagger import *
